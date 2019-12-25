d7a83825ab77464b3fa8262684d7adf3a772200a
Gavaar <oxspit@gmail.com>
2019-12-25 18:59:17 +0100

    firestore and pwa installed

--- projects/moyney/src/app/app.module.ts
+++ projects/moyney/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+  ],
+  providers: [AngularFireAuth],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- projects/moyney/src/environments/environment.ts
+++ projects/moyney/src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyDqITlFFAxUT-INFvLlMXji5zbz4ZzJq5o',
+    authDomain: 'moyney-balancer.firebaseapp.com',
+    databaseURL: 'https://moyney-balancer.firebaseio.com',
+    projectId: 'moyney-balancer',
+    storageBucket: 'moyney-balancer.appspot.com',
+    messagingSenderId: '550325569702',
+    appId: '1:550325569702:web:37f5101754f65ddd',
+  },
 };
 
 /*

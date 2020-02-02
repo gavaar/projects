bf89a2c2300d0b9a29a47c04dea6b25cfb95a9aa
Gavaar <oxspit@gmail.com>
2020-02-02 07:52:02 +0100

    before update

--- projects/moyney/src/app/app-routing.module.ts
+++ projects/moyney/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: '**', pathMatch: 'full', redirectTo: '' },
 ];
 
 @NgModule({

7127cf42a9deef0e573aa302e83396f177503d0c
Gavaar <oxspit@gmail.com>
2021-03-30 02:06:48 +0200

    call_ana style changes and updated changelog

--- projects/call-analyzer/src/assets/static/changelog.ts
+++ projects/call-analyzer/src/assets/static/changelog.ts
@@ -2,6 +2,14 @@ import { Changelog } from '@libs/moy-changelog/moy-changelog.module';
 
 export const CHANGELOG: Changelog[] = [
   {
+    version: '0.0.2',
+    changes: [
+      'Created MoyTable 2 to improve table maintainability and performance',
+      'Updated uploader component. Columns are gotten from the CSV now, and filters are appropiately set on left side for it',
+      'Persistence of selected filters on DataBase for future use.'
+    ]
+  },
+  {
     version: '0.0.1',
     changes: [
       'Created base app structure',

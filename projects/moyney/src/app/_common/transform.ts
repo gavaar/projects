0c4c89b2afa4c574f18f28196123f722d1030a56
Gavaar <oxspit@gmail.com>
2021-04-08 18:21:20 +0200

    call_ana deleted moyney from this project

--- projects/moyney/src/app/_common/transform.ts
+++
@@ -1,6 +0,0 @@
-/** Transform a Date object into a readable d/m/yyyy */
-function parseDateReadable(date: Date): string {
-  return date.toLocaleString('en-GB', { year: '2-digit', month: 'short', day: '2-digit' });
-}
-
-export { parseDateReadable };
